977811f2c2bc0eecfe59f32ef24f9622bd4da622
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2026-02-17 01:57:27 +0530

    Update page.tsx

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -7,11 +7,27 @@ import Footer from "@/components/footer"
 
 export default function ExplorePage() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen flex bg-[#0a0a0a]">
       <Header />
+       <div className="flex-1 flex flex-col h-[calc(100vh-16px)] relative m-2  bg-[#121212] rounded-xl border border-white/10 overflow-hidden transition-[margin,border-radius] duration-500 ease-[cubic-bezier(0.16,1,0.3,1)]">
+         <style>
+    {`
+      .hide-scrollbar::-webkit-scrollbar {
+        display: none;
+      }
+    `}
+  </style>
+        <div className="overflow-scroll hide-scrollbar"
+         style={{
+    scrollbarWidth: 'none',     // Firefox
+    msOverflowStyle: 'none'     // IE/Edge legacy
+  }}
+        >
       <ExploreHero />
       <ExploreFeatures />
       <Footer />
+      </div>
+      </div>
     </main>
   )
 }
