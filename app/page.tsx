12e85208fb6bb7aab3a84a1e67c1819c2fa08cdf
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2026-02-17 01:56:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,29 @@ import Footer from "@/components/footer"
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <main className="min-h-screen flex  bg-[#0a0a0a]">
       <Header />
-      <Hero />
-      <Features />
-      <Tools />
-      <Downloads />
-      <Footer />
+      <div className="flex-1 flex flex-col h-[calc(100vh-16px)] relative m-2 bg-[#121212] rounded-xl border border-white/10 overflow-hidden transition-[margin,border-radius] duration-500 ease-[cubic-bezier(0.16,1,0.3,1)]">
+         <style>
+    {`
+      .hide-scrollbar::-webkit-scrollbar {
+        display: none;
+      }
+    `}
+  </style>
+        <div className="overflow-scroll hide-scrollbar"
+         style={{
+    scrollbarWidth: 'none',     // Firefox
+    msOverflowStyle: 'none'     // IE/Edge legacy
+  }}
+        >
+        <Hero />
+        <Features />
+        <Tools />
+        <Downloads />
+        <Footer />
+        </div>
+      </div>
     </main>
   )
 }
